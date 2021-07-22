d735baaf00afb288f47b32a32984bca62bc509e5
raghvendra240 <raghvendragautam240@gmail.com>
2021-07-22 22:45:28 +0530

    update news list

---
+++ app/src/main/java/com/news/droiddebo/mytimes/MyTimesApplication.java
@@ -0,0 +1,18 @@
+package com.news.droiddebo.mytimes;
+
+import android.app.Application;
+
+/*
+** Used for getting the application instance
+**/
+public class MyTimesApplication extends Application {
+    private static MyTimesApplication myTimesApplicationInstance;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        myTimesApplicationInstance = this;
+    }
+    public static MyTimesApplication getMyTimesApplicationInstance(){
+        return myTimesApplicationInstance;
+    }
+}
